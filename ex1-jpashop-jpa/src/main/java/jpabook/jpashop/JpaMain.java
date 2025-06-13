e8e07ef02eae72d48300e07d2bef0b226c25752e
rlatjddms <kseo_o@naver.com>
2025-06-13 14:45:04 +0900

    실전 예제 4 - 상속관계 매핑

--- ex1-jpashop-jpa/src/main/java/jpabook/jpashop/JpaMain.java
+++ ex1-jpashop-jpa/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -19,13 +21,11 @@ public class JpaMain {
 
         try {
 
-            Order order = new Order();
-            em.persist(order);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
 
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
--- ex1-jpashop-jpa/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ ex1-jpashop-jpa/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Delivery {
+public class Delivery extends BaseEntity {
 
     @Id @GeneratedValue
     private Long id;
