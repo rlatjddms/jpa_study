864e97fb25c252762f8fadb698d38ac8f2ac6a3c
rlatjddms <kseo_o@naver.com>
2025-06-13 16:00:09 +0900

    즉시 로딩과 지연 로딩

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,16 +18,34 @@ public class JpaMain {
 
         try {
 
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(teamA);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
             
             em.flush();
             em.clear();
             
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-            Hibernate.initialize(refMember); //강제 초기화
+//            Member m = em.find(Member.class, member1.getId());
+
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
+
+            //SQL: select * from Member
+            //SQL: select * from Team where TEAM_ID = xxx
 
             tx.commit();
         } catch (Exception e) {
